63339165bcb775be485d97fe378305aa45c380d8
Abhinay Reddy <mail2abhinaygaddam6@gmail.com>
2026-07-07 22:12:58 +0530

    Add files via upload

---
+++ File Handing/ReadFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class ReadFile {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("student.txt");
+
+        int ch;
+
+        while ((ch = fr.read()) != -1) {
+            System.out.print((char) ch);
+        }
+
+        fr.close();
+    }
+}
